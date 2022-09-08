09de93c7bb8a56f628069848ca59823f6430afc1
Jennie Duffer <jenniej297@gmail.com>
2022-09-08 16:05:25 -0500

    initial commit

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _conn = connection;
+        }
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _conn.Execute("INSERT INTO PRODUCTS (Name, Price, CategoryID) VALUES (name, price, @categoryID)", 
+                new {name = name, price = price, categoryID = categoryID});
+        }
+
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products");
+        }
+
+               public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM products Where ProductID = @id",
+                new { id = id });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price, CategoryID = @categoryID, OnSale = @onsale, StockLevel = @stocklevel " +
+                "Where ProductID = @id",
+            new { name = product.Name, price = product.Price, categoryID = product.CategoryID, onsale = product.OnSale, stocklevel = product.StockLevel, id = product.ProductID }) ;
+        }
+        public void DeleteProduct(int id)
+        {
+            _conn.Execute("DELETE From sales Where ProductID = @id", new { id = id });
+            _conn.Execute("DELETE From reviews Where ProductID = @id", new { id = id });
+            _conn.Execute("DELETE From products Where ProductID = @id", new { id = id });
+        }
+    }
+
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using BestBuyBestPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+departmentRepo.InsertDepartment("Jennie's New Department");
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach (var department in departments)
+{
+    Console.WriteLine(department.DepartmentID);
+    Console.WriteLine(department.Name);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+var productRepo = new DapperProductRepository(conn);
+
+//var productToUpdate = productRepo.GetProduct(940);
+
+//productToUpdate.Name = "Updated!!";
+//productToUpdate.Price = 14.99;
+//productToUpdate.CategoryID = 1;
+//productToUpdate.OnSale = true;
+//productToUpdate.StockLevel = 1000;
+//productRepo.UpdateProduct(productToUpdate);
+
+productRepo.DeleteProduct(940);
+
+var products = productRepo.GetAllProducts();
+foreach (var product in products)
+{
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.StockLevel);
+    Console.WriteLine();
+    Console.WriteLine();
+
+
+}
